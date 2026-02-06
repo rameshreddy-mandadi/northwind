b4354dfedd23ea749057b514a0f94436774f4a7d
Ramesh Reddy Mandadi <rameshreddy.mandady@gmail.com>
2026-02-06 16:25:51 +0000

    `Initial development`

---
+++ northwind/webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("northwind.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
